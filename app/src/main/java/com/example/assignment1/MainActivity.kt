ed87e4530cbfd9a79369fcc72edb7f5c56918a8a
VatsalBoricha <113151273+VatsalBoricha@users.noreply.github.com>
2024-02-03 02:49:46 -0500

    added processOperatorButtons shared handler

--- app/src/main/java/com/example/assignment1/MainActivity.kt
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.assignment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clearButton = findViewById<Button>(R.id.clearButton)
+        val percentButton = findViewById<Button>(R.id.percentButton)
+
+        clearButton.setOnClickListener{view -> processOperatorButtons(view)}
+        percentButton.setOnClickListener{view -> processOperatorButtons(view)}
+
+
+    }
+
+   private  fun processOperatorButtons(view: View)
+    {
+       val resultTextView = findViewById<TextView>(R.id.resultTextView)
+         Log.i("operators", view.tag.toString())
+        resultTextView.text = view.tag.toString()
     }
 }
